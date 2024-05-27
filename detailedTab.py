f7253865b4fae5655e85896250d8eda104cb9ce1
crapa-Desktop <crapa36@gmail.com>
2024-05-27 22:43:55 +0900

    Update detailedTab.py

--- detailedTab.py
+++ detailedTab.py
@@ -12,7 +12,8 @@ class DetailedTab:
         )  # frame을 부모로 가지는 Canvas 생성
         self.canvas.pack(expand=True, fill="both")  # Canvas를 확장하여 채우도록 설정
         # 지도탭
-        self.canvas.create_rectangle(20, 20, 800 - 20, 900 - 220, tags="Details")
+        self.canvas.create_rectangle(20, 20, 800 - 10, 900 - 220, tags="Details")
         # 주소 정보
-        self.canvas.create_rectangle(20, 900 - 200, 800 - 20, 900 - 20, tags="Details")
-        # 좌측 체크박스
+        self.canvas.create_rectangle(20, 900 - 200, 800 - 10, 900 - 20, tags="Details")
+        # 캠핑장 이름
+        self.canvas.create_rectangle(800 + 10, 20, 1600 - 20, 200 - 20, tags="Details")
