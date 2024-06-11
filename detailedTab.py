0dfd00b61c46ad2a35694f45c4cc3737b41beef0
2002OID <ykkim308321@gmail.com>
2024-06-11 13:05:45 +0900

    북마크에 추가기능

--- detailedTab.py
+++ detailedTab.py
@@ -2,35 +2,23 @@ from tkinter import *
 
 
 class DetailedTab:
-    def __init__(self, notebook):
+    def __init__(self, notebook, bookmarks):
         self.frame = Frame(notebook)
         notebook.add(self.frame, text="세부정보")
 
-
+        self.bookmarks = bookmarks  # 북마크 리스트 저장
         self.address = '강원도 춘천시 남면 가옹개길 52-9'
-
         self.wep_address = 'http://outofpark.com/main/'
-
         self.call_num = '1522-1861-'
-
         self.way_to_book = '온라인실시간예약'
-
         self.name = '(주)아웃오브파크'
-
         self.description = '아웃오브파트는 강원도 춘천시 남면에 자리했다. 서울양양고속도로 강촌IC에서 엘리시안강촌 방면으로 30분가량 달리면 도착한다. 이곳은 북 한강 변의 수려한 풍광을 배경으로 캐러밴 40대가 들어찼다. 고급스러움이 돋보이는 유럽피안 캐러밴과 에어스트림 캐러밴이다. 모든 캐러밴은 각기 다른 주제로 꾸몄다. 이 덕분에 욕실에 중점을 둔 객실이나 침실에 초점을 맞춘 객실 등 취향에 따라 선택하는 재미가 있다. 외부에는 어닝 아래  테이블, 의자, 노천욕탕, 바비큐 시설을 마련했다. 캠핑장의 강점 중 하나는 부대시설이다. 카페, 수영장, 찜질방, 스파, 중앙 무대, 분수, 노래방 등 고급스러움으로 치장한 시설이 차고 넘친다.'
-
         self.category = '카라반'
-
         self.position = '산,강'
-
         self.season = '봄,여름,가을,겨울'
-
         self.date = '평일+주말'
-
         self.can_animal = '불가능'
-
         self.additional_facilities = '운동시설'
-
         self.brazier_stand = '개별'
 
         self.canvas = Canvas(
@@ -125,4 +113,5 @@ class DetailedTab:
             self.can_animal_label.config(text='동물 동반 여부: ' + campsite['animalAllow'])
             self.additional_facilities_label.config(text='부대시설: ' + campsite['amenities'])
             self.brazier_stand_label.config(text='화로대: ' + campsite['brazier'])
-
+    def add_to_bookmarks(self, campsite):
+        self.bookmarks.append(campsite)
