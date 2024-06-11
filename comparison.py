f433f29835c5dc69fec29ce1f9e5ba5b9c10dd81
2002OID <ykkim308321@gmail.com>
2024-06-11 15:17:30 +0900

    수정중

--- comparison.py
+++ comparison.py
@@ -6,6 +6,9 @@ class Comparison:
         self.frame = Frame(notebook)
         notebook.add(self.frame, text="비교하기")
 
+        self.bookmark_list = bookmark_list
+
+
         self.canvas = Canvas(
             self.frame, width=1600, height=900, bg="white"
         )  # frame을 부모로 가지는 Canvas 생성
@@ -15,16 +18,17 @@ class Comparison:
         self.canvas.create_rectangle(20, 20, 800 - 20, 900 - 20, tags="Left")
         left_frame = Frame(self.frame)  # 부모 위젯을 self.frame으로 변경
         left_frame.place(x=20, y=20)
-        self.bookmark_listBox = ttk.Combobox(left_frame, values=bookmark_list, width=105)
+        self.bookmark_listBox = ttk.Combobox(left_frame, width=105)
         self.bookmark_listBox.pack(side=LEFT)
 
         # 우측 콤보박스
         self.canvas.create_rectangle(800 + 20, 20, 1600 - 20, 900 - 20, tags="Left")
         right_frame = Frame(self.frame)  # 부모 위젯을 self.frame으로 변경
         right_frame.place(x=800 + 20, y=20)
-        self.bookmark_listBox = ttk.Combobox(right_frame, values=bookmark_list, width=105)
+        self.bookmark_listBox = ttk.Combobox(right_frame, width=105)
         self.bookmark_listBox.pack(side=LEFT)
 
-
+        for N in self.bookmark_list:
+            self.bookmark_listBox.insert(END, N['name'])
 
 
