0e0cddc01eaac849106797777149664d8c28e391
2002OID <ykkim308321@gmail.com>
2024-06-11 15:17:59 +0900

    선택 시 상세 설명 전환

--- bookmark.py
+++ bookmark.py
@@ -27,12 +27,25 @@ class Bookmark:
         self.bookmark_listBox.config(yscrollcommand=bookmark_scroll.set)
         self.bookmark_listBox.bind('<<ListboxSelect>>', self.on_select)
 
+        # 이름, 설명
+        self.canvas.create_rectangle(400 + 20, 20, 1600 - 20, 200 - 20, tags="name")
+        name_frame = Frame(self.frame, bg='white')
+        name_frame.place(x=400 + 40, y=20 + 20)
+
+        self.name_label = Label(name_frame, font=30, bg='white')
+        self.name_label.pack(side=TOP, anchor='w')
+
+        self.description_label = Label(name_frame, wraplength=720, justify=LEFT, bg='white')
+        self.description_label.pack(side=TOP)
+        #상세
+        self.canvas.create_rectangle(400 + 20, 200 + 20, 1600 - 20, 900 - 20, tags="Details")
+        details_frame = Frame(self.frame, bg='white')
+        details_frame.place(x=400 + 40, y=200 + 40)
 
         for N in self.bookmark_list:
             self.bookmark_listBox.insert(END, N['name'])
 
-        # 상세항목
-        self.canvas.create_rectangle(400 + 20, 20, 1600 - 20, 200 - 20, tags="Details")
+
 
     def on_select(self, event):
         # 선택된 항목의 인덱스 가져오기
@@ -43,5 +56,7 @@ class Bookmark:
 
         # 선택된 캠핑장의 정보 출력 (또는 다른 작업 수행)
         print(selected_campsite)
+        self.name_label.config(text=selected_campsite['name'])
+        self.description_label.config(text=selected_campsite['intro'])
 
 
