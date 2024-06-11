8c6629a6395deefef78b3d199edb52cb64093add
2002OID <ykkim308321@gmail.com>
2024-06-12 00:36:23 +0900

    비교를 위한 선택 가능

--- comparison.py
+++ comparison.py
@@ -28,6 +28,27 @@ class Comparison:
         self.bookmark_listBoxR = ttk.Combobox(right_frame, width=105)
         self.bookmark_listBoxR.pack(side=LEFT)
 
+        # 콤보박스에 이벤트 바인드
+        self.bookmark_listBoxL.bind('<<ComboboxSelected>>', self.on_select_left)
+        self.bookmark_listBoxR.bind('<<ComboboxSelected>>', self.on_select_right)
+    def on_select_left(self, event):
+        # 선택된 항목의 인덱스 가져오기
+        index = self.bookmark_listBoxL.current()
+
+        # 선택된 캠핑장의 정보 가져오기
+        selected_campsite = self.bookmark_list[index]
+
+        # 선택된 캠핑장의 정보 출력 (또는 다른 작업 수행)
+        print(selected_campsite)
+    def on_select_right(self, event):
+        # 선택된 항목의 인덱스 가져오기
+        index = self.bookmark_listBoxR.current()
+
+        # 선택된 캠핑장의 정보 가져오기
+        selected_campsite = self.bookmark_list[index]
+
+        # 선택된 캠핑장의 정보 출력 (또는 다른 작업 수행)
+        print(selected_campsite)
 
     def update(self, bookmark_list):
         self.bookmark_list = bookmark_list
